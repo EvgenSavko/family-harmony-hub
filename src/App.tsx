eac8896695be500ef8f1292da55a93e451140a59
yevhen.savko <yevhen.savko@anvileight.com>
2024-11-04 11:26:44 +0200

    feat: add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {auth} from './firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 function App() {
+  const [email, setEmail] = React.useState("");
+const [password, setPassword] = React.useState("");
+
+  const handleLogin = async () => {
+    await createUserWithEmailAndPassword(auth, email, password)
+    setEmail("")
+    setPassword("")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input placeholder="Email..." onChange={e => setEmail(e.target.value)}/>
+<input placeholder="Passwon..." type="password" onChange={e => setPassword(e.target.value)}/>
+<button onClick={handleLogin}>login</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -19,6 +33,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+
+</div>
     </div>
   );
 }
