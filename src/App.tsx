08854c1f6af942dd5c3f387b9d77a8da47b540da
yevhen.savko <yevhen.savko@anvileight.com>
2024-11-04 11:57:01 +0200

    feat: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {auth} from './firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from './firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 function App() {
-  const [email, setEmail] = React.useState("");
-const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
   const handleLogin = async () => {
-    await createUserWithEmailAndPassword(auth, email, password)
-    setEmail("")
-    setPassword("")
-  }
+    await createUserWithEmailAndPassword(auth, email, password);
+    setEmail('');
+    setPassword('');
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input placeholder="Email..." onChange={e => setEmail(e.target.value)}/>
-<input placeholder="Passwon..." type="password" onChange={e => setPassword(e.target.value)}/>
-<button onClick={handleLogin}>login</button>
+        <input
+          placeholder="Email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Passwon..."
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={handleLogin}>login</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -34,9 +41,7 @@ const [password, setPassword] = React.useState("");
         </a>
       </header>
 
-      <div>
-
-</div>
+      <div></div>
     </div>
   );
 }
